a0e0714fee1fdae3f7b725f2532d50ec6a3cf052
zohaib <hafizzohaibamin@gmail.com>
2023-05-08 16:24:38 +0500

    first commit

---
+++ app/src/main/java/com/example/lightversion/LightSplash.kt
@@ -0,0 +1,11 @@
+package com.example.lightversion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LightSplash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_light_splash)
+    }
+}
